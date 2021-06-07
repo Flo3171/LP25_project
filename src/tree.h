f1ebb6a904827491ca12b2d3434f43346e1c2b4e
gfantou <guillaume.fantou@utbm.fr>
2021-06-07 19:40:13 +0200

    Tree

---
+++ src/tree.h
@@ -0,0 +1,37 @@
+#ifndef _TREE_H
+#define _TREE_H
+
+#include <stddef.h>
+#include <linux/limits.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MD5_DIGEST_LENGTH 50
+#define NAME_MAX 50
+
+typedef enum {DIRECTORY, REGULAR_FILE, OTHER_TYPE} e_type;
+
+typedef struct _file {
+    e_type file_type;
+    char name[NAME_MAX+1];
+    time_t mod_time;
+    uint64_t file_size;
+    u_char md5sum[MD5_DIGEST_LENGTH];
+    struct _file *next_file;
+} s_file;
+
+typedef struct _directory {
+    char name[NAME_MAX+1];
+    time_t mod_time;
+    struct _directory *subdirs;
+    s_file *files;
+    struct _directory *next_dir;
+} s_directory;
+
+int append_subdir(s_directory *child, s_directory *parent);
+int append_file(s_file *child, s_directory *parent);
+void clear_files(s_directory *parent);
+void clear_subdirs(s_directory *parent);
+
+#endif
